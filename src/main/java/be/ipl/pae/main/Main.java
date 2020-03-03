b1a060b6845ce059da7349f5a471bf180fe8327d
Grégoire de Crombrugghe <gregoire.decrombrugghe@student.vinci.be>
2020-03-03 12:21:13 +0100

    push test

--- src/main/java/be/ipl/pae/main/Main.java
+++ src/main/java/be/ipl/pae/main/Main.java
@@ -3,7 +3,7 @@ package be.ipl.pae.main;
 public class Main {
 
   public static void main(String[] args) throws Exception {
-    System.out.println("Hello world !");
+    System.out.println("Hello world test !");
   }
 
 }
