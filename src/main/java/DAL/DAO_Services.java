7bb6e757cf8f0a207095a8928b4119f831dc0ca1
Ali Koksal <ali.koksal@student.vinci.be>
2020-03-03 14:10:31 +0100

    feat: début implémention DAL

---
+++ src/main/java/DAL/DAO_Services.java
@@ -0,0 +1,40 @@
+package DAL;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class DAO_Services {
+
+  private String url = "jdbc:postgresql://172.24.2.6:5432/dbmariabouraga";
+  private Connection conn = null;
+
+  public DAO_Services() {
+
+
+    try {
+      Class.forName("org.postgresql.Driver");
+    } catch (ClassNotFoundException e) {
+      System.out.println("Driver PostgreSQL manquant !");
+      System.exit(1);
+    }
+    try {
+      conn = DriverManager.getConnection(url, "mariabouraga", "S3CIMX1NU");
+    } catch (SQLException e) {
+      System.out.println("Impossible de joindre le server !");
+      System.exit(1);
+    }
+
+  }
+
+  public PreparedStatement tryPreparedSatement(PreparedStatement ps) {
+    try {
+      ps = conn.prepareStatement("SELECT * FROM projet.clients WHERE email = ?");
+    } catch (SQLException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+    return ps;
+  }
+}
