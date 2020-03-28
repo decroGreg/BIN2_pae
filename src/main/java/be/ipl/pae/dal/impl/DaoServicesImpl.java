5a5e78dc8f38a0d1ac12d00f9e270c527894ed48
Ali Koksal <ali.koksal@student.vinci.be>
2020-03-28 19:13:07 +0100

    feat: ajout du multithreading au projet (coté DAL): 	  création DaoServicesUCC , Ajout des methodes demarrerTransaction, 	  commit , rollback

--- src/main/java/be/ipl/pae/dal/impl/DaoServicesImpl.java
+++ src/main/java/be/ipl/pae/dal/impl/DaoServicesImpl.java
@@ -1,17 +1,16 @@
 
 package be.ipl.pae.dal.impl;
 
-import be.ipl.pae.biz.config.Config;
-import be.ipl.pae.dal.interfaces.DaoServices;
-
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-
+import org.apache.commons.dbcp2.BasicDataSource;
 import be.ipl.pae.biz.config.Config;
+import be.ipl.pae.dal.interfaces.DaoServices;
+import be.ipl.pae.dal.interfaces.DaoServicesUCC;
+import be.ipl.pae.exceptions.DALException;
 
-public class DaoServicesImpl implements DaoServices {
+public class DaoServicesImpl implements DaoServices, DaoServicesUCC {
 
   // private String url = "jdbc:postgresql://127.0.0.1/Projet";
   // private String url = "jdbc:postgresql://172.24.2.6:5432/dbmariabouraga";
@@ -22,38 +21,84 @@ public class DaoServicesImpl implements DaoServices {
   private String url = (String) conf.getConfigPropertyAttribute("db.url");
   private String login = (String) conf.getConfigPropertyAttribute("db.login");
   private String mdp = (String) conf.getConfigPropertyAttribute("db.mdp");
-  private Connection conn = null;
+  private BasicDataSource ds;
+  private ThreadLocal<Connection> connections;
+
 
   public DaoServicesImpl() {
-
+    connections = new ThreadLocal<Connection>();
     try {
-      Class.forName("org.postgresql.Driver");
-    } catch (ClassNotFoundException e) {
-      System.out.println("Driver PostgreSQL manquant !");
-      System.exit(1);
-    }
-    try {
-      // conn = DriverManager.getConnection(url, "postgres", "Greg171598");
-      // conn = DriverManager.getConnection(url, "mariabouraga", "S3CIMX1NU");
-      conn = DriverManager.getConnection(url, login, mdp);
-
-    } catch (SQLException e) {
+      ds = new BasicDataSource();
+      ds.setDriverClassName("org.postgresql.Driver");
+      ds.setUrl(url);
+      ds.setUsername(login);
+      ds.setPassword(mdp);
+      // Class.forName("org.postgresql.Driver");
+    } catch (Exception e) {
       System.out.println("Impossible de joindre le server !");
       System.exit(1);
     }
+    /**
+     * try { // conn = DriverManager.getConnection(url, "postgres", "Greg171598"); // conn =
+     * DriverManager.getConnection(url, "mariabouraga", "S3CIMX1NU"); conn =
+     * DriverManager.getConnection(url, login, mdp);
+     * 
+     * } catch (SQLException e) { System.out.println("Impossible de joindre le server !");
+     * System.exit(1); }
+     **/
 
   }
 
   @Override
-  public PreparedStatement getPreparedSatement(String requeteSQL) {
-    PreparedStatement ps = null;
+  public void demarrerTransaction() {
+    if (connections.get() != null) {
+      throw new DALException("Transaction déja ouverte");
+    }
     try {
-      ps = conn.prepareStatement(requeteSQL);
+      Connection conn = this.ds.getConnection();
+      connections.set(conn);
+      conn.setAutoCommit(false);
     } catch (SQLException e) {
-      // TODO Auto-generated catch block
       e.printStackTrace();
     }
+  }
 
-    return ps;
+  @Override
+  public void commit() {
+    try (Connection conn = connections.get()) {
+      if (conn == null) {
+        throw new DALException("Ne peut pas commit car il n'y a pas de transaction");
+      }
+      conn.commit();
+      conn.setAutoCommit(true);
+    } catch (SQLException e) {
+      e.printStackTrace();
+    } finally {
+      connections.remove();
+    }
+  }
+
+  @Override
+  public void rollback() {
+    try (Connection conn = connections.get()) {
+      if (conn == null) {
+        throw new DALException("Ne peut pas rollback car il n'y a pas de transaction");
+      }
+      conn.rollback();
+      conn.setAutoCommit(true);
+    } catch (SQLException e) {
+      throw new DALException("Erreur dans le rollback");
+    } finally {
+      connections.remove();
+    }
+  }
+
+  @Override
+  public PreparedStatement getPreparedSatement(String requeteSQL) {
+    try {
+      return connections.get().prepareStatement(requeteSQL);
+    } catch (SQLException e) {
+      throw new DALException("Erreur dans le prepared statement");
+    }
   }
 }
