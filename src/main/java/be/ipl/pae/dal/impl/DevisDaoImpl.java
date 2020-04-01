ed927513151ef3d1c6a2a35fbc22ece1232f0efb
Ali Koksal <ali.koksal@student.vinci.be>
2020-04-01 15:40:17 +0200

    fix: modification de confirmer date devis

--- src/main/java/be/ipl/pae/dal/impl/DevisDaoImpl.java
+++ src/main/java/be/ipl/pae/dal/impl/DevisDaoImpl.java
@@ -1,5 +1,11 @@
 package be.ipl.pae.dal.impl;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
 import be.ipl.pae.biz.dto.ClientDto;
 import be.ipl.pae.biz.dto.DevisDto;
 import be.ipl.pae.biz.factory.FactoryImpl;
@@ -8,13 +14,6 @@ import be.ipl.pae.biz.interfaces.Factory;
 import be.ipl.pae.dal.daoservices.DaoServices;
 import be.ipl.pae.dal.interfaces.DevisDao;
 import be.ipl.pae.exceptions.DalException;
-
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Timestamp;
-import java.util.ArrayList;
-import java.util.List;
 
 public class DevisDaoImpl implements DevisDao {
 
@@ -99,10 +98,12 @@ public class DevisDaoImpl implements DevisDao {
 
   @Override
   public boolean confirmerDateDevis(int idDevis, Timestamp dateDebutTravaux) {
-    String requeteSql = "UPDATE init.devis SET statut = 'DDI' WHERE id_devis = ?";
+    String requeteSql =
+        "UPDATE init.devis SET statut = 'DDI',date_debut_travaux = ? WHERE id_devis = ?";
     ps = services.getPreparedSatement(requeteSql);
     try {
-      ps.setInt(1, idDevis);
+      ps.setInt(2, idDevis);
+      ps.setTimestamp(1, dateDebutTravaux);
       ps.execute();
     } catch (SQLException ex) {
       throw new DalException("Erreur lors de la confirmation de la Date" + ex.getMessage());
