037394d8ed0e76598b0246c6bb2b8bfc3d270442
Ali Koksal <ali.koksal@student.vinci.be>
2020-04-28 14:38:28 +0200

    feat : ajout de la methode voir tous devis dans l'interface photoDao

--- src/main/java/be/ipl/pae/dal/interfaces/PhotoDao.java
+++ src/main/java/be/ipl/pae/dal/interfaces/PhotoDao.java
@@ -2,7 +2,12 @@ package be.ipl.pae.dal.interfaces;
 
 import be.ipl.pae.biz.dto.PhotoDto;
 
+import java.util.List;
+
 public interface PhotoDao {
 
   boolean introduirePhoto(PhotoDto photoDto);
+
+  List<PhotoDto> voirTousPhotos();
+
 }
