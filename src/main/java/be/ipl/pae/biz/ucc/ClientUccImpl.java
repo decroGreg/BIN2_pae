f186433a2d096f3f1c56d9773bcca3176b02b37d
Samuel Van Campenhout <samuel.vancampenhout@student.vinci.be>
2020-05-04 23:10:57 +0200

    feat: methode pour recuperer le nom de tous les users et clients

--- src/main/java/be/ipl/pae/biz/ucc/ClientUccImpl.java
+++ src/main/java/be/ipl/pae/biz/ucc/ClientUccImpl.java
@@ -72,4 +72,17 @@ public class ClientUccImpl implements ClientUcc {
       throw new FatalException(de.getMessage());
     }
   }
+
+  public List<String> getNomClients() {
+    List<String> noms;
+    try {
+      daoServicesUcc.demarrerTransaction();
+      noms = clientDao.nomClient();
+    } catch (DalException de) {
+      daoServicesUcc.rollback();
+      throw new FatalException(de.getMessage());
+    }
+    daoServicesUcc.commit();
+    return Collections.unmodifiableList(noms);
+  }
 }
