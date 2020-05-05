2618420f213d0bae1ed26ffb2d0d4b462e733a63
Gregoire de Crombrugghe <gregoire.decrombrugghe@student.vinci.be>
2020-05-05 08:03:35 +0200

    fix:checkStyle error on Response

--- src/main/java/be/ipl/pae/ihm/response/ResponseImpl.java
+++ src/main/java/be/ipl/pae/ihm/response/ResponseImpl.java
@@ -6,7 +6,12 @@ import javax.servlet.http.HttpServletResponse;
 
 public class ResponseImpl implements Response {
 
-
+  /**
+   * 
+   * @param resp l httpServletRexponse
+   * @param json le json des donnees a renvoyer
+   * @throws IOException
+   */
   public static void success(HttpServletResponse resp, String json) throws IOException {
     System.out.println("JSON generated : success");
 
@@ -21,6 +26,13 @@ public class ResponseImpl implements Response {
 
   }
 
+  /**
+   * 
+   * @param resp l httpServletRexponse
+   * @param message le message a afficher
+   * @throws IOException
+   */
+
   public static void raterRequete(HttpServletResponse resp, String message) throws IOException {
     resp.setContentType("application/json");
     resp.setCharacterEncoding("UTF-8");
@@ -30,6 +42,12 @@ public class ResponseImpl implements Response {
     resp.getWriter().write(json);
   }
 
+  /**
+   * 
+   * @param resp l httpServletRexponse
+   * @param excl'exception
+   * @throws IOException
+   */
   public static void errorServer(HttpServletResponse resp, Exception exc) {
     resp.setContentType("application/json");
     resp.setCharacterEncoding("UTF-8");
@@ -38,9 +56,9 @@ public class ResponseImpl implements Response {
         + HttpServletResponse.SC_INTERNAL_SERVER_ERROR + exc.getMessage() + "\"}";
     try {
       resp.getWriter().write(json);
-    } catch (IOException e) {
+    } catch (IOException exce) {
       // TODO Auto-generated catch block
-      e.printStackTrace();
+      exce.printStackTrace();
     }
   }
 }
